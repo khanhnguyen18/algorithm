130faddf4af0bf726b267235440eca34edf45ccf
Khanh Nguyen <outdoor1988@gmail.com>
2025-05-19 23:48:25 +0700

    123123

--- hash-table/src/main/java/TwoSumSolution.java
+++ hash-table/src/main/java/TwoSumSolution.java
@@ -1,9 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSumSolution {
 
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
 
-        int[] result = new int[5];
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
 
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
+            }
+
+            numMap.put(nums[i], i);
+        }
+
+        return new int[0]; // No solution found
     }
 }
--- hash-table/src/test/java/TwoSumSolutionTest.java
+++ hash-table/src/test/java/TwoSumSolutionTest.java
@@ -5,12 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumSolutionTest {
 
-    private TwoSumSolution twoSumSolution = new TwoSumSolution();
+    private TwoSumSolution solution = new TwoSumSolution();
 
     @Test
-    void twoSum() {
-        int[] result = new int[5];
+    public void testExample1() {
 
-        assertEquals(null, twoSumSolution.twoSum(result, 7));
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, solution.twoSum(nums, target));
+    }
+
+    @Test
+    public void testExample2() {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, solution.twoSum(nums, target));
+    }
+
+    @Test
+    public void testExample3() {
+
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expected = {0, 1};
+        assertArrayEquals(expected, solution.twoSum(nums, target));
     }
 }
